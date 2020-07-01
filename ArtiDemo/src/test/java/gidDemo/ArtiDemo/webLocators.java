dbd40dd56ab74ce142a3b060887eb15160af3172
ebsadmin <ebsadmin@LAP0160.droisys.local>
2020-07-01 23:00:09 +0530

    second commit

--- ArtiDemo/src/test/java/gidDemo/ArtiDemo/webLocators.java
+++ ArtiDemo/src/test/java/gidDemo/ArtiDemo/webLocators.java
@@ -6,11 +6,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class webLocators {
 
 	public static void main(String[] args) {
-		
-		System.setProperty("webdriver.chrome.driver", "E:\\Softwares\\Testing Softwares\\chromedriver_win32\\E:\\Softwares\\Testing Softwares\\chromedriver.exe");
-		
+
+		System.setProperty("webdriver.chrome.driver",
+				"E:\\Softwares\\Testing Softwares\\chromedriver_win32\\E:\\Softwares\\Testing Softwares\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com");
+		driver.manage().window().maximize();
 
 	}
 
