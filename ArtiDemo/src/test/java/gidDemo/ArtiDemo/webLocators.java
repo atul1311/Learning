4aaca6fa6cf90e115ba92506503abf5974d6cfdb
ebsadmin <ebsadmin@LAP0160.droisys.local>
2020-07-01 20:16:46 +0530

    First commit - Whole Project

---
+++ ArtiDemo/src/test/java/gidDemo/ArtiDemo/webLocators.java
@@ -0,0 +1,17 @@
+package gidDemo.ArtiDemo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class webLocators {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "E:\\Softwares\\Testing Softwares\\chromedriver_win32\\E:\\Softwares\\Testing Softwares\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+
+	}
+
+}
